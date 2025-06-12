82c6d5c4cc04a46f7c019eda8d788912d65b66c8
Harshroxnox <harshroxnox@gmail.com>
2025-06-13 00:27:42 +0530

    added sample .env

--- backend/config.py
+++ backend/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 class Config:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")  
-    SECRET_KEY = os.getenv("SECRET_KEY", "default")
+    TEXTRAZOR_API_KEY = os.getenv("TEXTRAZOR_API_KEY")
